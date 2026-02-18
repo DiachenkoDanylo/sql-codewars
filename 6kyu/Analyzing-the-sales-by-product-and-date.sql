760bb8169ab23c49fe7ee56c01fa4df7693ab61e
Diachenko Danylo <moyo6699@gmail.com>
2026-02-18 12:56:48 +0200

    Analyzing the sales by product and date

---
+++ 6kyu/Analyzing-the-sales-by-product-and-date.sql
@@ -0,0 +1,54 @@
+-- Task
+-- Given the information about sales in a store, calculate the total revenue for each day, month, year, and product.
+--
+-- Notes
+-- The sales table stores only the dates for which any data has been recorded - the information about individual sales (what was sold, and when) is stored in the sales_details table instead
+-- The sales_details table stores totals per product per date
+-- Order the result by the product_name, year, month, day columns
+-- We're interested only in the product-specific data, so you shouldn't return the total revenue from all sales
+-- Input tables
+--         ----------------------------------------
+--         |    Table      |   Column   |  Type   |
+--         |---------------+------------+---------|
+--         | products      | id         | int     |
+--         |               | name       | text    |
+--         |               | price      | numeric |
+--         |---------------+------------+---------|
+--         | sales         | id         | int     |
+--         |               | date       | date    |
+--         |---------------+------------+---------|
+--         | sales_details | id         | int     |
+--         |               | sale_id    | int     |
+--         |               | product_id | int     |
+--         |               | count      | int     |
+--         -----------------------------------------
+-- Output table
+--         --------------------------
+--         |    Column    |  Type   |
+--         |--------------+---------|
+--         | product_name | text    |
+--         | year         | int     |
+--         | month        | int     |
+--         | day          | int     |
+--         | total        | numeric |
+--         --------------------------
+-- Example output
+-- product_name | year | month | day | total
+--     -------------+------+-------+-----+------
+--      milk        | 2019 | 01    | 01  | 200
+--      milk        | 2019 | 01    | 02  | 190
+--      milk        | 2019 | 01    |     | 390
+--      milk        | 2019 | 02    | 01  | 240
+--      milk        | 2019 | 02    |     | 240
+--      milk        | 2019 |       |     | 630
+--      milk        |      |       |     | 630
+--     -------------+------+-------+-----+------
+
+-- extract(year, month, day from [date_column or ] date '1909-12-05')
+-- rollup postgresql 9+
+-- A common use of  ROLLUP is to calculate the aggregations of data by year, month, and date,
+-- considering the hierarchy year > month > date
+
+select p.name as "product_name", extract(year from s.date) as "year", extract(month from s.date) as "month", extract(day from s.date) as "day", sum(p.price*s.count) as total
+    from products p join (select s.*, sd.* from sales s join sales_details sd on s.id=sd.sale_id) s on p.id=s.product_id group by rollup("product_name","year", "month", "day")
+    having p.name is not null order by "product_name", "year", "month", "day";
